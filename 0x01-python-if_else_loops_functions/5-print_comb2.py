6c965bce1370ee640866d5096ffd1b13d556547f
mouadnait <moadnait6@gmail.com>
2023-08-08 12:05:52 +0100

    5. 00...99

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 for i in range(0, 99):
     if len(str(i)) == 1:
-        print("0{}".format(i), end=', ')
+        print("{:>02}".format(i), end=', ')
     else:
         print("{}".format(i), end=', ')
 print(99)
