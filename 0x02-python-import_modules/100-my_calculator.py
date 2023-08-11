b701d7df9eb9f0b0d16273439a1e89bfc1e3d86a
mouadnait <moadnait6@gmail.com>
2023-08-11 01:27:46 +0100

    6. Build my own calculator!

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,28 +1,14 @@
 #!/usr/bin/python3
-import sys
-from calculator_1 import add, sub, mul, div
-if __name__ == '__main__':
-    list_operations = ['+', '-', '*', '/']
-    if len(sys.argv)-1 == 3 and sys.argv[2] in list_operations:
-        if sys.argv[2] == '+':
-            print("{} {} {} = {}".format(
-                sys.argv[1], sys.argv[2], sys.argv[3],
-                add(int(sys.argv[1]), int(sys.argv[3]))))
-        elif sys.argv[2] == '-':
-            print("{} {} {} = {}".format(
-                sys.argv[1], sys.argv[2], sys.argv[3],
-                sub(int(sys.argv[1]), int(sys.argv[3]))))
-        elif sys.argv[2] == '*':
-            print("{} {} {} = {}".format(
-                sys.argv[1], sys.argv[2], sys.argv[3],
-                mul(int(sys.argv[1]), int(sys.argv[3]))))
-        elif sys.argv[2] == '/':
-            print("{} {} {} = {}".format(
-                sys.argv[1], sys.argv[2], sys.argv[3],
-                div(int(sys.argv[1]), int(sys.argv[3]))))
-    elif len(sys.argv) == 1:
-        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
-        sys.exit(1)
-    else:
-        print("Unknown operator. Available operators: +, -, * and /")
-        sys.exit(1)
+if __name__ == "__main__":
+    from sys import argv
+    from calculator_1 import add, sub, mul, div
+    if len(argv) != 4:
+        print("Usage:", argv[0], "<a> <operator> <b>")
+        exit(1)
+
+    a, operand, b = int(argv[1]), argv[2], int(argv[3])
+    arith = {'+': add, '-': sub, '*': mul, '/': div}
+    if operand not in arith:
+        print("Unknown operator. Only: +, -, * and / available")
+        exit(1)
+    print('{:d} {} {:d} = {:d}'.format(a, operand, b, arith[operand](a, b)))
