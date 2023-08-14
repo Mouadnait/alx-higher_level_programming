facd77165f376a53343eec8751491bd8d44f82e3
mouadnait <moadnait6@gmail.com>
2023-08-14 20:27:20 +0100

    2. Replace element

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
 def replace_in_list(my_list, idx, element):
-    my_list[idx] = element if len(my_list) > idx >= 0 else my_list
-    return my_list
+    if 0 <= idx < len(my_list):
+        my_list[idx] = element
+    return(my_list)
