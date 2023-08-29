8135472decd256d27083d4e99c5e754001293dd1
mouadnait <moadnait6@gmail.com>
2023-08-29 20:22:02 +0100

    1. Square with size

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Define a class Square."""
+class Square:
+    """
+    Class that defines properties of square by: (based on 0-square.py).
+
+    Attributes:
+        size: size of a square (1 side).
+    """
+    def __init__(self, size):
+        """
+        size : no type/value verification
+        ----------------------------
+        Private instance attribute size
+        """
+        self.size = size
