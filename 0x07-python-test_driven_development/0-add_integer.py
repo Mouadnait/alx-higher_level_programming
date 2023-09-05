b36e6314504ad99d4bb072f25001a284921260ae
mouadnait <moadnait6@gmail.com>
2023-09-05 20:43:47 +0100

    0x07. Python - Test-driven development

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""Defines an integer addition function."""
+
+
+def add_integer(a, b=98):
+    """Adds two integers.
+
+    Args:
+        a: first int.
+        b: second int, default value is 98.
+
+    Raises:
+        TypeError: if a, b are neither int nor float.
+
+    Returns:
+        sum of a and b.
+    """
+    if not isinstance(a, int) and not isinstance(a, float):
+        raise TypeError("a must be an integer")
+    if not isinstance(b, int) and not isinstance(b, float):
+        raise TypeError("b must be an integer")
+    add = lambda x, y: int(x) + int(y) if isinstance(x, float) or isinstance(y, float) else x + y
+    result = add(a, b)
+    return result
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testfile("tests/0-add_integer.txt")
