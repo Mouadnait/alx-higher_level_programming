310433c9e1cf76a3d5900503b9bf3f4f17db79d6
mouadnait <moadnait6@gmail.com>
2023-09-23 23:30:45 +0100

    7. Display #1

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -149,7 +149,7 @@ class Rectangle(Base):
         for _ in range(self.__y):
             print()
         for _ in range(self.__height):
-            print(" " * self.__y + '#' * self.__width)
+            print(" " * self.__x + '#' * self.__width)
 
     def update(self, *args, **kwargs):
         """
