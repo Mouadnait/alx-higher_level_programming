fd394632b557eb146dfc5702b0976157179329e1
mouadnait <moadnait6@gmail.com>
2023-10-17 23:36:01 +0100

    Edit task 15 <Number by score>

--- 0x0D-SQL_introduction/15-groups.sql
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -1,4 +1,5 @@
 -- List the number of records with the same score in the second table
-SELECT score, COUNT(score) AS number
+SELECT score, COUNT(*) AS number
 FROM second_table
-GROUP BY score DESC;
+GROUP BY score
+ORDER BY score DESC;
