6987fcba999436ca125641e2f234918913b060fe
Mouad Nait si <91064574+Mouadnait@users.noreply.github.com>
2023-10-17 23:58:59 +0100

    Edit task 7 <insert_value.sql>

--- 0x0D-SQL_introduction/7-insert_value.sql
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -1,3 +1,3 @@
 -- Inserts a new row in the table
 -- Using INSERT to insert a new table
-INSERT INTO first_table (`id`, `name`) VALUES (89, "Holberton School");
+INSERT INTO first_table (`id`, `name`) VALUES (89, "Best School");
