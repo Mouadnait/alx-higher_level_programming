142808ad8eec9d82f7ecf0bc872effda9e9b62b1
mouadnait <moadnait6@gmail.com>
2023-11-14 00:29:48 +0100

    11. Second biggest

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,9 +1,10 @@
 #!/usr/bin/node
-const args = process.argv.slice(2);
-if (args.length <= 1) {
-    console.log(0);
+const process = require('process');
+const argv = process.argv;
+const argc = process.argv.length;
+if (argc < 4) {
+  console.log(0);
 } else {
-    const numbers = [...new Set(args.map(Number))];
-    const sortedNumbers = numbers.sort((a, b) => b - a);
-    console.log(sortedNumbers[1]);
+  const array = argv.slice(2).sort((a, b) => a - b).reverse();
+  console.log(array[1]);
 }
