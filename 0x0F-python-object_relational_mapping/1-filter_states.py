8bee00cb7bc4a8156c40df2eb8d4c0a538fd3f73
mouadnait <moadnait6@gmail.com>
2023-11-19 23:34:25 +0100

    0x0F. Python - Object-relational mapping

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -2,19 +2,22 @@
 """"lists all states with a name starting with N (upper N)
 from the database hbtn_0e_0_usa
 """
+import MySQLdb
+import sys
+
 if __name__ == "__main__":
-    import MySQLdb
-    import sys
-    conn = MySQLdb.connect(
+    db = MySQLdb.connect(
         host="localhost",
         port=3306,
-        user=sys.argv[1],
-        passwd=sys.argv[2],
-        db=sys.argv[3])
-    cur = conn.cursor()
+        user=argv[1],
+        passwd=argv[2],
+        db=argv[3],
+        charset="utf8")
+    cur = db.cursor()
     cur.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
-    query_rows = cur.fetchall()
-    for row in query_rows:
-        print(row)
+    rows = cur.fetchall()
+    for row in rows:
+        if row[1][0] == 'N':
+            print(row)
     cur.close()
-    conn.close()
+    db.close()
--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -2,22 +2,23 @@
 """takes in an argument and displays all values in the
 states table of hbtn_0e_0_usa where name matches the argument.
 """
+import MySQLdb
+from sys import argv
+
 if __name__ == "__main__":
-    import MySQLdb
-    import sys
-    conn = MySQLdb.connect(
+    db = MySQLdb.connect(
         host="localhost",
         port=3306,
-        user=sys.argv[1],
-        passwd=sys.argv[2],
-        db=sys.argv[3])
-    cur = conn.cursor()
-    cur.execute(
-        "SELECT * FROM states WHERE name LIKE '%%{userinput}%%' \
-ORDER BY id ASC".format(
-            userinput=sys.argv[4]))
-    query_rows = cur.fetchall()
-    for row in query_rows:
-        print(row)
+        user=argv[1],
+        passwd=argv[2],
+        db=argv[3],
+        charset="utf8")
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states WHERE name LIKE '{:s}' ORDER BY \
+    id ASC".format(argv[4]))
+    rows = cur.fetchall()
+    for row in rows:
+        if row[1] == argv[4]:
+            print(row)
     cur.close()
-    conn.close()
+    db.close()
