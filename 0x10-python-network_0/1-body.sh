37d5132fbeda3e13f5871cfa3187729ad72b229c
mouadnait <moadnait6@gmail.com>
2023-12-01 00:18:41 +0100

    1. cURL to the end

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,8 +1,3 @@
 #!/bin/bash
-<<<<<<< HEAD
-# send a GET request and display the body of the response for a 200 status code
-curl -sL "$1"
-=======
 # displays request body only on status code 200
 curl -sL "$@"
->>>>>>> 4c3bdf2ca79087458f6394d56e4a70132fc706ca
