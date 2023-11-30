2499db918be56b528725d01f16d29e180fc09b91
mouadnait <moadnait6@gmail.com>
2023-12-01 00:18:58 +0100

    0. cURL body size

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,8 +1,3 @@
 #!/bin/bash
-<<<<<<< HEAD
-# send a resquest and display the size of the response body
-curl -sI "$1" | grep -i Content-Length | awk '{print $2}'
-=======
 # gets the length of the body of an html get request
 curl -sI "$1" | grep -i Content-Length | awk '{print $2}'
->>>>>>> 4c3bdf2ca79087458f6394d56e4a70132fc706ca
