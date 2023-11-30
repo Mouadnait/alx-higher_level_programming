0e93b9e53cdb6d5551efb17c9da72163ec7f01f2
mouadnait <moadnait6@gmail.com>
2023-11-30 22:48:10 +0100

    0. cURL body size

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # send a resquest and display the size of the response body
-curl -sI $1 | grep -i Content-length | awk '{print $2}'
+curl -sI "$1" | grep -i Content-Length | awk '{print $2}'
