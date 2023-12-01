44869584a2af7d451adcb8e06d9d446c50efc0c6
mouadnait <moadnait6@gmail.com>
2023-12-02 00:02:32 +0100

    4. What's my status? #1

---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+Python script that fetches https://alx-intranet.hbtn.io/status
+"""
+import requests
+
+
+if __name__ == "__main__":
+    url = 'https://alx-intranet.hbtn.io/status'
+    with requests.get(url) as response:
+        print("Body response:\n"
+            "\t- type: {}\n"
+            "\t- content: {}".format(
+                type(response.text), response.text))
