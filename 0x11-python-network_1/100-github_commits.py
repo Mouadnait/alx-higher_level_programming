9082debd989add7f24f8b17d6209e2b97dd11271
mouadnait <moadnait6@gmail.com>
2023-12-02 02:21:31 +0100

    10. Time for an interview

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+script that takes 2 arguments in order to list 10 commits (from the most
+recent to oldest) of the repository "rails" by the user "rails".
+Print all commits by: `<sha>: <author name>` (one by line)
+The first argument will be the repository name
+The second argument will be the owner name
+"""
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    repository_name = sys.argv[1]
+    owner_name = sys.argv[2]
+    url = f"https://api.github.com/repos/{owner_name}/{repository_name}/commits"
+    try:
+        response = requests.get(url)
+        commits = response.json()
+        for i, obj in enumerate(commits):
+            if i == 10:
+                break
+            if type(obj) is dict:
+                name = obj.get('commit').get('author').get('name')
+                print("{}: {}".format(obj.get('sha'), name))
+    except Exception as invalid_json:
+        pass
