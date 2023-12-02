491f491166f4ecf9f52520467a2866c1d9ec79cb
mouadnait <moadnait6@gmail.com>
2023-12-02 02:34:23 +0100

    10. Time for an interview

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -14,7 +14,7 @@ if __name__ == "__main__":
     repository_name = sys.argv[1]
     owner_name = sys.argv[2]
     url = "https://api.github.com/repos/{}/{}/commits".format(
-        repository_name, owner_name)
+        owner_name, repository_name)
     try:
         response = requests.get(url)
         commits = response.json()
