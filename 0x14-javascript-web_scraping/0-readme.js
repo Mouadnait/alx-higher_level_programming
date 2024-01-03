949fca4a22c6167ba8d1fa9dbbc71881bd72f9c0
mouadnait <moadnait6@gmail.com>
2024-01-03 22:39:08 +0100

    0. Readme

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,2 +1,11 @@
 #!/usr/bin/node
+const fs = require('fs');
 
+// Get the file path from the command line arguments
+const filePath = process.argv[2];
+// Read the file asynchronously with utf-8 encoding
+if (filePath) {
+    fs.readFile(filePath, 'utf-8', (err, data) => {
+        err ? console.log(err) : console.log(file);
+    });
+}
