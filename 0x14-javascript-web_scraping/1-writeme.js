e782a0aa0aa6253a2608f941003d26a5402545db
mouadnait <moadnait6@gmail.com>
2024-01-03 23:01:22 +0100

    1. Write me

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,1 +1,9 @@
 #!/usr/bin/node
+const fs = require('fs');
+
+// The first argument is the file path
+const filePath = process.argv[2];
+// The second argument is the string to write
+const contentToWrite = process.argv[3];
+// Write to the file asynchronously with utf-8 encoding
+fs.writeFile(filePath, contentToWrite, 'utf-8', (err) => err ? console.log(err) : '');
