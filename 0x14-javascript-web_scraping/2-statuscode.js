aef094ec530400365169743d4989763afe97b8fb
mouadnait <moadnait6@gmail.com>
2024-01-03 23:11:59 +0100

    2. Status code

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,1 +1,10 @@
 #!/usr/bin/node
+const process = require('process');
+const request = require('request');
+
+// The first argument is the URL to request (GET)
+const url = process.argv[2];
+// Make a GET request and display the status code
+request.get(url, (error, response) => {
+    error ? console.log(error) : console.log(`code: ${response.statusCode}`);
+});
